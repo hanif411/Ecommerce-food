c4c1392520ab90010f72e9e1a58f0654409d29c7
hanif <hanifsholihin884@gmail.com>
2025-12-27 16:49:11 +0700

    fix bug order no user

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -1,6 +1,7 @@
 import { orderService } from "@/services/orderService";
+import { userService } from "@/services/userService";
 import { useCartStore } from "@/store/useCartStore";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 import { toast } from "sonner";
@@ -8,6 +9,12 @@ import { toast } from "sonner";
 export const useCheckout = () => {
   const cartStore = useCartStore();
   const router = useRouter();
+
+  const { data: user } = useQuery({
+    queryKey: ["profile"],
+    queryFn: userService.getUser,
+    retry: false,
+  });
 
   const mutation = useMutation({
     mutationFn: orderService.createOrder,
@@ -37,6 +44,11 @@ export const useCheckout = () => {
 
   const handleCheckout = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!user) {
+      toast.error("Silahkan login terlebih dahulu untuk melakukan checkout");
+      router.push("/login");
+      return; 
+    }
 
     const formdata = new FormData(e.currentTarget);
     const data = Object.fromEntries(formdata);
